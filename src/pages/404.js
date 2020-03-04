37ef5f1e27126a72cffdf718d805c664b6bde79a
Rouncer27 <trevor@switchbackcreative.ca>
2020-03-04 09:58:30 -0700

    404 and google ver codes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,33 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/allSite/seo/seo"
+import styled from "styled-components"
+
+import { buttonOne } from "../Utilities"
+
+const StyledNotFound = styled.section`
+  width: 100%;
+  max-width: 75rem;
+  margin-top: 5rem;
+  margin-right: auto;
+  margin-bottom: 5rem;
+  margin-left: auto;
+  padding: 2.5rem;
+
+  a {
+    ${buttonOne};
+  }
+`
 
 const NotFoundPage = props => (
   <Layout location={props.location.pathname}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <StyledNotFound>
+      <h1>NOT FOUND</h1>
+      <p>Sorry, but the page you are looking for cannot be found.</p>
+      <Link to="/">Go Back Home</Link>
+    </StyledNotFound>
   </Layout>
 )
 
