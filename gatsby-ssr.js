67e4097e8182481256f4e6f30af618ef440c64c8
Trevor Rounce <trevor@switchbackcreative.ca>
2025-07-16 12:36:42 -0600

    added GTM

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from "react"
 
-// You can delete this file if you're not using it
+// 1️⃣ Inject the GTM script into the <head>
+export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
+  setHeadComponents([
+    <script
+      key="gtm-head"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TBKFQ34K');`,
+      }}
+    />,
+  ])
+
+  // 2️⃣ Inject the <noscript> GTM iframe just after <body>
+  setPreBodyComponents([
+    <noscript
+      key="gtm-noscript"
+      dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBKFQ34K"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      }}
+    />,
+  ])
+}
